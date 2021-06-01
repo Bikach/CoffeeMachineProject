60c74048d97bb0b0d63e1652c902979604d2c4f9
Chakib Houd <choud@oui.sncf>
2021-06-01 17:40:52 +0200

    refactoring DrinkMakerTest class

--- src/test/java/fr/chakib/houd/kata/unit/DrinkMakerTest.java
+++ src/test/java/fr/chakib/houd/kata/unit/DrinkMakerTest.java
@@ -1,6 +1,7 @@
 package fr.chakib.houd.kata.unit;
 
 import fr.chakib.houd.kata.domain.manufacture.DrinkMaker;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,66 +10,78 @@ class DrinkMakerTest {
 
     DrinkMaker drinkMaker = new DrinkMaker();
 
-    @Test
-    void shouldBeAbleToSendInstructionsForMakingATeaWithoutSugar() {
-        drinkMaker.order("T::");
+    @Nested
+    class WithoutSugar {
+        @Test
+        void shouldBeAbleToSendInstructionsForMakingATeaWithoutSugar() {
+            drinkMaker.order("T::");
 
-        var instructionsSent = drinkMaker.sendInstruction();
+            var instructionsSent = drinkMaker.sendInstruction();
 
-        assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with no sugar and therefore no stick");
+            assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with no sugar and therefore no stick");
+        }
+
+        @Test
+        void shouldBeAbleToSendInstructionsForMakingACoffeeWithoutSugar() {
+            drinkMaker.order("C::");
+
+            var instructionsSent = drinkMaker.sendInstruction();
+
+            assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with no sugar and therefore no stick");
+        }
+
+        @Test
+        void shouldBeAbleToSendInstructionsForMakingAChocolateWithoutSugar() {
+            drinkMaker.order("H::");
+
+            var instructionsSent = drinkMaker.sendInstruction();
+
+            assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with no sugar and therefore no stick");
+        }
+
     }
 
-    @Test
-    void shouldBeAbleToSendInstructionsForMakingACoffeeWithoutSugar() {
-        drinkMaker.order("C::");
+    @Nested
+    class WithSugar {
+        @Test
+        void shouldBeAbleToSendInstructionsForMakingATeaWitAtSugar() {
+            drinkMaker.order("T:1:0");
 
-        var instructionsSent = drinkMaker.sendInstruction();
+            var instructionsSent = drinkMaker.sendInstruction();
 
-        assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with no sugar and therefore no stick");
+            assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with 1 sugar and a stick");
+        }
+
+        @Test
+        void shouldBeAbleToSendInstructionsForMakingACoffeeWitAtSugar() {
+            drinkMaker.order("C:1:0");
+
+            var instructionsSent = drinkMaker.sendInstruction();
+
+            assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with 1 sugar and a stick");
+        }
+
+        @Test
+        void shouldBeAbleToSendInstructionsForMakingAChocolateWitAtSugar() {
+            drinkMaker.order("H:1:0");
+
+            var instructionsSent = drinkMaker.sendInstruction();
+
+            assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with 1 sugar and a stick");
+        }
     }
 
-    @Test
-    void shouldBeAbleToSendInstructionsForMakingAChocolateWithoutSugar() {
-        drinkMaker.order("H::");
+    @Nested
+    class CustomerInformation {
+        @Test
+        void shouldBeAbleToSendInstructionsToInformTheCustomerThatTheyWillReceiveAMessageForTheirOrder() {
+            drinkMaker.order("M:message-content");
 
-        var instructionsSent = drinkMaker.sendInstruction();
+            var instructionsSent = drinkMaker.sendInstruction();
 
-        assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with no sugar and therefore no stick");
+            assertThat(instructionsSent)
+                    .isEqualTo("Drink maker forwards any message received onto the coffee machine interface for the customer to see");
+        }
     }
 
-    @Test
-    void shouldBeAbleToSendInstructionsForMakingATeaWitAtSugar() {
-        drinkMaker.order("T:1:0");
-
-        var instructionsSent = drinkMaker.sendInstruction();
-
-        assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with 1 sugar and a stick");
-    }
-
-    @Test
-    void shouldBeAbleToSendInstructionsForMakingACoffeeWitAtSugar() {
-        drinkMaker.order("C:1:0");
-
-        var instructionsSent = drinkMaker.sendInstruction();
-
-        assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with 1 sugar and a stick");
-    }
-
-    @Test
-    void shouldBeAbleToSendInstructionsForMakingAChocolateWitAtSugar() {
-        drinkMaker.order("H:1:0");
-
-        var instructionsSent = drinkMaker.sendInstruction();
-
-        assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with 1 sugar and a stick");
-    }
-    @Test
-    void shouldBeAbleToSendInstructionsToInformTheCustomerThatTheMachineTransmitsAnyMessageReceivedOnTheInterfaceOfTheCoffeeMachine() {
-        drinkMaker.order("M:message-content");
-
-        var instructionsSent = drinkMaker.sendInstruction();
-
-        assertThat(instructionsSent)
-                .isEqualTo("Drink maker forwards any message received onto the coffee machine interface for the customer to see");
-    }
 }
