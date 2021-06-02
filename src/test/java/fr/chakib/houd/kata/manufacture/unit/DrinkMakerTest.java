abde8ab3e9decec9b6a0780cd3a1b2e6601c555a
Chakib Houd <choud@oui.sncf>
2021-06-02 11:12:22 +0200

    refactoring test - move the assertion into a generic method

--- src/test/java/fr/chakib/houd/kata/manufacture/unit/DrinkMakerTest.java
+++ src/test/java/fr/chakib/houd/kata/manufacture/unit/DrinkMakerTest.java
@@ -13,35 +13,37 @@ class DrinkMakerTest {
 
     DrinkMaker drinkMaker = new DrinkMaker();
 
+    private void assertsThatInstructionsSentAreCorrect(String order, String instruction) {
+        drinkMaker.order(new Protocol(order));
+        assertThat(drinkMaker.sendInstruction()).isEqualTo(instruction);
+    }
+
     @Nested
     class ShouldMakeADrink {
         @Nested
         class WithoutSugar {
             @Test
             void thenSendInstructionsThatContainsATeaWithoutStick() {
-                drinkMaker.order(new Protocol("T:0:0.4"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with no sugar and therefore no stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "T:0:0.4",
+                        "Drink maker makes 1 tea with no sugar and therefore no stick"
+                );
             }
 
             @Test
             void thenSendInstructionsThatContainsACoffeeWithoutStick() {
-                drinkMaker.order(new Protocol("C:0:0.6"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with no sugar and therefore no stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "C:0:0.6",
+                        "Drink maker makes 1 coffee with no sugar and therefore no stick"
+                );
             }
 
             @Test
             void thenSendInstructionsThatContainsAChocolateWithoutStick() {
-                drinkMaker.order(new Protocol("H:0:0.5"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with no sugar and therefore no stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "H:0:0.5",
+                        "Drink maker makes 1 chocolate with no sugar and therefore no stick"
+                );
             }
 
         }
@@ -50,29 +52,26 @@ class DrinkMakerTest {
         class WithSugar {
             @Test
             void thenSendInstructionsThatContainsATeaWithAStick() {
-                drinkMaker.order(new Protocol("T:1:0.4"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with 1 sugar and a stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "T:1:0.4",
+                        "Drink maker makes 1 tea with 1 sugar and a stick"
+                );
             }
 
             @Test
             void thenSendInstructionsThatContainsACoffeeWithAStick() {
-                drinkMaker.order(new Protocol("C:2:0.6"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with 2 sugar and a stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "C:2:0.6",
+                        "Drink maker makes 1 coffee with 2 sugar and a stick"
+                );
             }
 
             @Test
             void thenSendInstructionsThatContainsAChocolateWithAStick() {
-                drinkMaker.order(new Protocol("H:1:0.5"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with 1 sugar and a stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "H:1:0.5",
+                        "Drink maker makes 1 chocolate with 1 sugar and a stick"
+                );
             }
         }
 
@@ -80,29 +79,26 @@ class DrinkMakerTest {
         class WithExactAmount {
             @Test
             void forMakeATeaWithAnySugarNumber() {
-                drinkMaker.order(new Protocol("T:1:0.4"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 tea with 1 sugar and a stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "T:1:0.4",
+                        "Drink maker makes 1 tea with 1 sugar and a stick"
+                );
             }
 
             @Test
             void forMakeACoffeeWithAnySugarNumber() {
-                drinkMaker.order(new Protocol("C:0:0.6"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 coffee with no sugar and therefore no stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "C:0:0.6",
+                        "Drink maker makes 1 coffee with no sugar and therefore no stick"
+                );
             }
 
             @Test
             void forMakeAChocolateWithAnySugarNumber() {
-                drinkMaker.order(new Protocol("H:4:0.5"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("Drink maker makes 1 chocolate with 4 sugar and a stick");
+                assertsThatInstructionsSentAreCorrect(
+                        "H:4:0.5",
+                        "Drink maker makes 1 chocolate with 4 sugar and a stick"
+                );
             }
         }
     }
@@ -110,47 +106,43 @@ class DrinkMakerTest {
 
     @Nested
     class ShouldNotMakeADrink {
+
         @Nested
         class WithAnInsufficientAmount {
             @Test
             void thenSendInstructionAboutTheMissingAmountForaTea(){
-                drinkMaker.order(new Protocol("T:2:0.2"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("There are 0.20 cents missing to make a tea.");
+                assertsThatInstructionsSentAreCorrect(
+                        "T:2:0.2",
+                        "There are 0.20 cents missing to make a tea."
+                );
             }
-
             @Test
             void thenSendInstructionAboutTheMissingAmountForaCoffee(){
-                drinkMaker.order(new Protocol("C:0:0.1"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("There are 0.50 cents missing to make a coffee.");
+                assertsThatInstructionsSentAreCorrect(
+                        "C:0:0.1",
+                        "There are 0.50 cents missing to make a coffee."
+                );
             }
 
             @Test
             void thenSendInstructionAboutTheMissingAmountForaChocolate(){
-                drinkMaker.order(new Protocol("H:1:0.3"));
-
-                var instructionsSent = drinkMaker.sendInstruction();
-
-                assertThat(instructionsSent).isEqualTo("There are 0.20 cents missing to make a chocolate.");
+                assertsThatInstructionsSentAreCorrect(
+                        "H:1:0.3",
+                        "There are 0.20 cents missing to make a chocolate."
+                );
             }
+
         }
     }
-
     @Nested
     class CustomerInformation {
+
         @Test
         void shouldBeAbleToSendInstructionsToInformTheCustomerThatTheyWillReceiveAMessageForTheirOrder() {
-            drinkMaker.order(new Protocol("M:message-content"));
-
-            var instructionsSent = drinkMaker.sendInstruction();
-
-            assertThat(instructionsSent)
-                    .isEqualTo("Drink maker forwards any message received onto the coffee machine interface for the customer to see");
+            assertsThatInstructionsSentAreCorrect(
+                    "M:message-content",
+                    "Drink maker forwards any message received onto the coffee machine interface for the customer to see"
+            );
         }
     }
 }
